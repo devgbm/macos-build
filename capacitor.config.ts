398f23e8e58f87b00d5f88965e3bad429753f0bc
Gabriel Maia <gabrielmaia.ti@outlook.com>
2025-11-19 15:09:07 -0400

    adicionado projeto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.gbmaia.teste',
+  appName: 'mob',
+  webDir: 'dist/mob/browser'
+};
+
+export default config;
